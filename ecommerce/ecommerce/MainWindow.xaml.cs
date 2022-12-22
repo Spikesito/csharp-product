d575df9dabb5ac8771fe34af45b04b977c5c46b8
Spikesito <emileseguret@icloud.com>
2022-12-22 16:51:49 +0100

    test tsé

--- ecommerce/ecommerce/MainWindow.xaml.cs
+++ ecommerce/ecommerce/MainWindow.xaml.cs
@@ -12,6 +12,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySqlConnector;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Security.Cryptography;
+///using System.Windows.Forms;
 
 namespace ecommerce
 {
@@ -23,6 +30,26 @@ namespace ecommerce
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string server = "localhost";
+            string database = "products";
+            string uid = "root";
+            string password = "";
+            string connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            MySqlConnection cnn;
+            cnn = new MySqlConnection(connectionString);
+            cnn.Open();
+            MessageBox.Show("Connection Open !");
+            cnn.Close();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
---
+++ ecommerce/ecommerce/Views/Login.xaml.cs
@@ -0,0 +1,43 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ecommerce.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour Login.xaml
+    /// </summary>
+    public partial class Login : UserControl
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        
+            string server = "localhost";
+            string database = "products";
+            string uid = UserTextBox.Text;
+            string password = PassBox.Password;
+            string connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            MySqlConnection cnn = new MySqlConnection(connectionString);
+
+
+            ///MessageBox.Show("Username cannot be empty.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
