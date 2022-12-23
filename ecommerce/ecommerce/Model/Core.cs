bc6d683a05573df837d31cd3991c9c9d4bea7192
WSAIOUDI <wassim.saioudi@ynov.com>
2022-12-23 14:04:16 +0100

    login in MVVM

---
+++ ecommerce/ecommerce/Model/Core.cs
@@ -0,0 +1,35 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecommerce.Model
+{
+    public class Core
+    {
+        private string password;
+        private string uid;
+
+        public Core(string pd, string u)
+        {
+            password = pd;
+            uid = u;
+        }
+
+        public MySqlConnection ConnectDB()
+        {
+            string connectionString = "SERVER=localhost;DATABASE=ecom-products;UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            MySqlConnection cnn = new MySqlConnection(connectionString);
+            return cnn;
+        }
+
+    // string server = "localhost";
+    // string database = "ecom-products";
+    // string uid = UserTextBox.Text;
+    // string password = PassBox.Password;
+}
+ }
+
